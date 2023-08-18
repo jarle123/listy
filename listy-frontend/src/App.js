5fadb391223e661eea7b6230a0dbc92f8d4ba565
Jarle Farstad <jarle.farstad@capgemini.com>
2023-08-18 14:04:24 +0200

    Updated react-router-dom 5 -> 6

--- listy-frontend/src/App.js
+++ listy-frontend/src/App.js
@@ -4,60 +4,59 @@ import React, { Component } from "react";
 import Settings from "./components/Settings.js";
 import Footer from "./components/Footer.js";
 import MainView from "./components/MainView.js";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import axios from "axios";
 
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      listys: [],
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            listys: [],
+        };
+    }
 
-  componentDidMount() {
-    axios.get(apiUrl + "ListyLists").then((response) => {
-      this.setState({
-        listys: response.data,
-      });
-    });
-  }
+    componentDidMount() {
+        axios.get(apiUrl + "ListyLists").then((response) => {
+            this.setState({
+                listys: response.data,
+            });
+        });
+    }
 
-  render() {
-    return (
-      <div className="app">
-        <Router>
-          <header className="app-header">
-            <div>
-              <h1>Listy</h1>
+    render() {
+        return (
+            <div className="app">
+                <BrowserRouter>
+                    <header className="app-header">
+                        <div>
+                            <h1>Listy</h1>
+                        </div>
+                        <nav id="main-navigation">
+                            <ul>
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+
+                                <li>
+                                    <Link to="/settings">Settings</Link>
+                                </li>
+                                <li>
+                                    <Link to="/boards">Boards</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </header>
+                    <div className="app-content">
+                        <Routes>
+                            <Route exact path="/" element={<MainView />} />
+                            <Route path="/settings" element={<Settings />} />
+                        </Routes>
+                    </div>
+                </BrowserRouter>
+                <Footer></Footer>
             </div>
-            <nav id="main-navigation">
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-
-                <li>
-                  <Link to="/settings">Settings</Link>
-                </li>
-                <li>
-                  <Link to="/boards">Boards</Link>
-                </li>
-              </ul>
-            </nav>
-          </header>
-          <div className="app-content">
-            <Switch>
-              <Route exact path="/" component={MainView} />
-              <Route path="/settings" component={Settings} />
-            </Switch>
-          </div>
-        </Router>
-        <Footer></Footer>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 const apiUrl = "https://localhost:44302/api/";
